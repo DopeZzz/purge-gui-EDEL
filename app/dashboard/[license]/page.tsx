3ad9c6156125607b75eb1060fbc97bc4a76c5134
Edel <121950752+edelgzlez@users.noreply.github.com>
2025-07-25 20:28:01 +0200

    Updated page.tsx

--- app/dashboard/[license]/page.tsx
+++ app/dashboard/[license]/page.tsx
@@ -579,8 +579,7 @@ export default function DashboardPage() {
         clearTimeout(debounceTimeoutRef.current)
       }
     }
-  },
-  [
+  ], [
     selectedWeapon,
     selectedScope,
     selectedBarrel,
